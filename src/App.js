79e601f393d6395bf543ea3dee6e3dfb01858d53
Rabindra Joshi <therj@users.noreply.github.com>
2018-07-10 02:09:49 +0545

    initial table :art:

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './App.css'
+import { Table } from 'reactable'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Table
+          className="table"
+          data={[
+            { Name: 'Griffin Smith', Age: 18 },
+            { Age: 23, Name: 'Lee Salminen' },
+            { Age: 28, Position: 'Developer' },
+          ]}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
